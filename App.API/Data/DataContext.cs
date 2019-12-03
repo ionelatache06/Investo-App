188935691b2b5d9ea6fcd14c9126cab1002e1567
Ionela Tache <ionelatache06@yahoo.com>
2019-12-03 20:38:11 +0200

    Initial Commit

---
+++ App.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using App.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){
+      
+        }
+        public DbSet<Value> Values{get; set;}
+    }
+}
