4875667f0661f80c7b96b7ae48935544936e2ee2
Ionela Tache <ionelatache06@yahoo.com>
2019-12-31 04:16:26 +0200

    Libraries

--- App-Ang/src/app/nav/nav.component.ts
+++ App-Ang/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import {AlertifyService} from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,28 +11,26 @@ export class NavComponent implements OnInit {
 
   model: any={};
 
-  constructor(private authService:AuthService) { }
+  constructor(public authService:AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login(){
     this.authService.login(this.model).subscribe(next=>{
-      console.log('The log in was successfull');
+      this.alertify.success('The log in was successfull');
     }, error=> {
-      console.log('Log in failed')
+      this.alertify.error('error')
     });
   }
 
   loggedin(){
-    const token = localStorage.getItem('token');
-    return !!token;
-    //returns true only if there is somethiing in the token
-  }
+    return this.authService.loggedIn();
+  }  
 
   logout(){
     localStorage.removeItem('token');
-    console.log('loggedout');
+    this.alertify.message('loggedout');
   }
 
 }
